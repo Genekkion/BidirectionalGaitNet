108d57e5f39575f9ef292caa0e4e7246dee694dd
genekkion <genekkion@live.com>
2024-06-06 21:17:01 +0800

    testing

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# ENVDIR=${ENVDIR:-~/pkgenv}
-ENVDIR=${ENVDIR:-/home/user/pkgenv}
+ENVDIR=${ENVDIR:-~/pkgenv}
 # If CentOS server, set C++ compiler manually
 if [ -f /etc/redhat-release ]; then
     export CC=/opt/ohpc/pub/compiler/gcc/8.3.0/bin/gcc
